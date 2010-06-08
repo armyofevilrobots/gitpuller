da2a2220326784e639ca46fcf471203acc6e56a2
Derek Anderson <derek@armyofevilrobots.com>
2010-06-08 16:10:57 -0700

    Remove comments

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 require_once('lib/gitpuller.inc.php');
-//echo $HTTP_RAW_POST_DATA; 
-//$push = json_decode($HTTP_RAW_POST_DATA);
-//var_dump($push);
-
-/*
-$head = explode("/", $push->ref);
-echo "<br/>Head of this was ".$head[count($head)-1]."<br/>\n";
-echo "Owner was ".$push->repository->owner->name."<br/>\n";
-echo "Project was ".$push->repository->name."<br/>\n";
- */
 $G= new GitPuller();
 $G->dumpcfg();
 $G->process($HTTP_RAW_POST_DATA);
